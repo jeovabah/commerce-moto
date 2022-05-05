1702a954d1a3be30068212d0f73092b7ee6578eb
jeovabah <jrjeova@hotmail.com>
2022-05-05 12:43:00 -0300

    Tela de Login

---
+++ loja/src/Pages/LoginPage/style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+    height: 100vh;
+    width: 100%;
+    display:flex;
+    flex-direction: row-reverse;
+    align-items: center;
+    flex-wrap: wrap;
+    position: absolute;
+    top: 50%;
+    transform: translate(0, -50%);
+    
+    .content {
+        width: 50%;
+        display:flex;
+        height: 100vh;
+        img { 
+            max-width: 100%;
+        }
+    }
+    @media screen and (max-width: 768px) {
+        .content {
+            width: 100%;
+        }
+    }
+`
--- loja/src/components/Login/style.ts
+++ loja/src/components/Login/style.ts
@@ -3,16 +3,17 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    
+    width: 100%;
     align-items: center;
     justify-content: center;
+
     padding:  0 2rem;
 
-    
-
     box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+
     background-color: rgba(255, 255, 255, 0.1);
-    top: 20px;
+
+    
     h3 {
         margin: 2rem;
         margin-bottom: 1rem;
