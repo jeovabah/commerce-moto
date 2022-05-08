079001008c373d1f81bc5240f0b886707be3210f
joaperi <jrjeova@hotmail.com>
2022-05-08 16:38:13 -0300

    Produto

---
+++ loja/src/Pages/ProductPage/style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    padding: 0 2rem;
+    margin: 1rem;
+
+`
---
+++ loja/src/data.ts
@@ -0,0 +1,53 @@
+ const data= {
+    productData: [
+        {
+            title: "Biz 125",
+            marca: "Honda",
+            cc: "124,9",
+            img: "https://www.motoremaza.com.br/Modelos/City/BIZ/Biz%20125/cores/BIZ125_Marrom-Perolizado_20210930.png?p=20210930",
+            price: 10000,
+            cart: 0 
+        },
+        {
+            title: "Ducati 1098",
+            marca: "Ducati",
+            cc: "1099",
+            img: "https://motoclube.com/files/artigos/top-20-melhores-motos-1.jpg",
+            price: 30941,
+            cart: 0 
+        },
+        {
+            title: "Triumph Street Triple",
+            marca: "Triumph",
+            cc: "675",
+            img: "https://motoclube.com/files/artigos/top-20-melhores-motos-2.jpg",
+            price: 18884,
+            cart: 0 
+        },
+        {
+            title: "Suzuki RGV250",
+            marca: "Suzuki",
+            cc: "249",
+            img: "https://motoclube.com/files/artigos/top-20-melhores-motos-4.jpg",
+            price: 17060,
+            cart: 0 
+        },
+        {
+            title: "KTM Super Duke 990",
+            marca: "KTM",
+            cc: "399",
+            img: "https://motoclube.com/files/artigos/top-20-melhores-motos-5.jpg",
+            price: 22952,
+            cart: 0 
+        },
+        {
+            title: "BMW R1200RT",
+            marca: "BMW",
+            cc: "1170",
+            img: "https://motoclube.com/files/artigos/top-20-melhores-motos-8.jpg",
+            price: 39575,
+            cart: 0 
+        }
+    ]
+}
+export default data;
